bd6d8af48985a794637378adaccfecc42cf18377
Daniel Armbruster <danielgundam00@gmail.com>
2022-03-28 15:22:14 -0600

    added header component

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -1,9 +1,26 @@
-
-
+import { useEffect } from "react";
+import Header from './Header.js';
 function App() {
+
+useEffect(()=>{
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com',
+  //     'X-RapidAPI-Key': '89810da5e7msh75b30d17b69208cp1d4af1jsne241ce2de733'
+  //   }
+  // };
+  
+  // fetch('https://alpha-vantage.p.rapidapi.com/query?from_currency=BTC&function=CURRENCY_EXCHANGE_RATE&to_currency=USD', options)
+  //   .then(response => response.json())
+  //   .then(response => console.log(response))
+  //   .catch(err => console.error(err));
+},[])
+
+
   return (
     <div className="App">
-   
+      <Header />
     </div>
   );
 }
---
+++ currency-converter/src/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+    <div className = "Header">
+        <h1>Crypto-Convert</h1>
+    </div>
+    )
+}
+
+export default Header
