ea33b5a393d606a8f08d54d5191158dbb0c3ba82
Daniel Armbruster <danielgundam00@gmail.com>
2022-03-30 13:23:43 -0600

    added a ton more css

--- currency-converter/src/Article.js
+++ currency-converter/src/Article.js
@@ -4,9 +4,9 @@ function Article({article}){
         <li className="article">
             <div className="card">
             <div className='the-card'>
-                <img src="https://source.unsplash.com/random/?Cryptocurrency" alt="cripto"/> 
+                <img className="imagesfromarticle" src="https://source.unsplash.com/random/?Cryptocurrency" alt="cripto"/> 
             <h4>{article.title}</h4>
-            <a href={article.url}>Click here to read article words</a>
+            <a className="articlelinks" href={article.url}>Click here to read article words</a>
             </div>
             </div>
         </li>
