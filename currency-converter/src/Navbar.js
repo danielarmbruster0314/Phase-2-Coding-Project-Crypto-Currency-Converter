aa658f39b50b09ee2d92dbd3747d5bea4f3739ab
Alexia Velador <alexiavel01@gmail.com>
2022-03-31 12:16:37 -0600

    nav bar chamges

--- currency-converter/src/Navbar.js
+++ currency-converter/src/Navbar.js
@@ -1,4 +1,5 @@
 import {NavLink} from "react-router-dom"
+import {useState} from 'react';
 
 const linkStyles = {
     display: "inline-block",
@@ -9,46 +10,107 @@ const linkStyles = {
     textDecoration: "none",
     color: "white",
   };
-  
-  
-
 
 function NavBar(){
+  const [style1, setStyle1] = useState({
+        margin: "40px",
+        color: "#d8adc7",
+        textDecoration: 'none',
+        textShadow: '2px 2px #bdac73'
+  });
+
+  const [style2, setStyle2] = useState({
+    margin: "40px",
+    color: "#d8adc7",
+    textDecoration: 'none',
+    textShadow: '2px 2px #bdac73'
+})
+
+const [style3, setStyle3] = useState({
+  margin: "40px",
+  color: "#d8adc7",
+  textDecoration: 'none',
+  textShadow: '2px 2px #bdac73'
+})
+
+
+  function handleMouseOver(){
+   setStyle1({
+    margin: "40px",
+    color: "#f6eac2",
+    textDecoration: 'none',
+    textShadow: '2px 2px #bdac73'})
+  }
+
+  function handleMouseOut(e){
+    setStyle1({margin: "40px",
+    color: "#d8adc7",
+    textDecoration: 'none',
+    textShadow: '2px 2px #bdac73'})
+  }
+
+  function handleMouseOver1(){
+    setStyle2({
+     margin: "40px",
+     color: "#f6eac2",
+     textDecoration: 'none',
+     textShadow: '2px 2px #bdac73'})
+   }
+ 
+   function handleMouseOut1(e){
+     setStyle2({margin: "40px",
+     color: "#d8adc7",
+     textDecoration: 'none',
+     textShadow: '2px 2px #bdac73'})
+   }
+
+   function handleMouseOver2(){
+    setStyle3({
+     margin: "40px",
+     color: "#f6eac2",
+     textDecoration: 'none',
+     textShadow: '2px 2px #bdac73'})
+   }
+ 
+   function handleMouseOut2(e){
+     setStyle3({margin: "40px",
+     color: "#d8adc7",
+     textDecoration: 'none',
+     textShadow: '2px 2px #bdac73'})
+   }
+
     return(
       <div className="navbar">
         <NavLink
         to="/"
         exact
-        style={{
-            margin: "5px 0px",
-            color: "rgba(27, 153, 139, 1)",
-            textDecoration: 'none',
-            textShadow: '2px 2px #bdac73'
-        }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        style={
+              style1
+        }
         className='navlink mainlink'>
           Main 
         </NavLink>
         <NavLink
         to="/converter"
         exact
-        style={{
-            margin: "5px 30px",
-            color: "rgba(27, 153, 139, 1)",
-            textDecoration: 'none',
-            textShadow: '2px 2px #bdac73'
-        }}
+        onMouseOver={handleMouseOver1}
+        onMouseOut={handleMouseOut1}
+        style={
+           style2       
+           }
         className ="navlink converterlink">
           Converter
         </NavLink>
         <NavLink
         to="/news"
         exact
-        style={{
-            margin: "5px 0px",
-            color: "rgba(27, 153, 139, 1)",
-            textDecoration: 'none',
-            textShadow: '2px 2px #bdac73'
-        }}
+        onMouseOver={handleMouseOver2}
+        onMouseOut={handleMouseOut2}
+        style={
+          style3
+        }
         className='navlink newslink'>
           Crytpo News
         </NavLink>
